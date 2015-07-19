7050696ec8bfeea767fd8430df921ec8614dedd7
aswonder <a.s.divov@mail.ru>
2015-07-19 20:52:34 +0300

    Add Connection class

---
+++ src/ru/asdivov/test/model/Connection.java
@@ -0,0 +1,47 @@
+package ru.asdivov.test.model;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Connection {
+
+    private OutputDevice outputDevice;
+    private List<InputDevice> inputDeviceList;
+
+    public Connection(OutputDevice outputDevice, InputDevice inputDevice) {
+        this.outputDevice = outputDevice;
+        inputDeviceList = new ArrayList<InputDevice>();
+        inputDeviceList.add(inputDevice);
+    }
+
+    public OutputDevice getOutputDevice() {
+        return outputDevice;
+    }
+
+    public void setOutputDevice(OutputDevice outputDevice) {
+        this.outputDevice = outputDevice;
+    }
+
+    public List<InputDevice> getInputDeviceList() {
+        return inputDeviceList;
+    }
+
+    public void setInputDeviceList(List<InputDevice> inputDeviceList) {
+        this.inputDeviceList = inputDeviceList;
+    }
+
+    public void add(InputDevice inputDevice) {
+        inputDeviceList.add(inputDevice);
+    }
+
+    public void remove(InputDevice inputDevice) {
+        inputDeviceList.remove(inputDevice);
+    }
+
+    @Override
+    public String toString() {
+        return outputDevice.toString() + " --> " + inputDeviceList.toString();
+    }
+
+}
